a6b6a6a02cd000cd569a3dd1f4bb60e960e05dca
SOONG-E <sksuwjd@naver.com>
2021-03-16 22:15:20 +0900

    opencv

--- transferLearning/sign_language.py
+++ transferLearning/sign_language.py
@@ -8,6 +8,8 @@ class_names = np.array(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', '
        'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
 
+hand_detector = cv2.CascadeClassifier('haarcascade_hand.xml')
+
 webcam = cv2.VideoCapture(0)
 
 if not webcam.isOpened():
@@ -29,6 +31,21 @@ while webcam.isOpened():
     predicted_class_name = class_names[predicted_class]
 
     cv2.putText(frame, predicted_class_name.title(), (60, 150), cv2.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 0), 3)
+
+    img = cv2.resize(frame, dsize=None, fx=1.0, fy=1.0)
+    #gray = np.float32(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # cascade 얼굴 탐지 알고리즘
+    results = hand_detector.detectMultiScale(gray,  # 입력 이미지
+                                       scaleFactor=1.1,  # 이미지 피라미드 스케일 factor
+                                       minNeighbors=5,  # 인접 객체 최소 거리 픽셀
+                                       minSize=(20, 20)  # 탐지 객체 최소 크기
+                                       )
+    for box in results:
+        x, y, w, h = box
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
 
     cv2.imshow('RPS', frame)
 
